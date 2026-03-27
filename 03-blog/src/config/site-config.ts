fb09e0d22ea38fa5dd91662fab4a35e896b50eb9
Patricio Osorio <patriciomiguel_12@hotmail.com>
2026-03-26 21:40:21 -0600

    Sección 5: Colecciones e imágenes - Estructura de nuestro blog

---
+++ 03-blog/src/config/site-config.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  title: 'My amazing blog',
+  description: 'A blog about amazing things',
+  author: 'Patricio Osorio',
+};
---
+++ 03-blog/src/interfaces/post-ui.interface.ts
@@ -0,0 +1,11 @@
+export interface IPostUI {
+  title: string;
+  excerpt: string;
+  href: string;
+  category: string;
+  image: string;
+  imageAlt: string;
+  publishedAt: string;
+  publishedAtISO: string;
+  readingTime: string;
+}
